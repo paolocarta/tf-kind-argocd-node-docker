8a9d9a504f2bd486115331509f01e3cb08575a3f
Paolo Carta <ing.pcarta@gmail.com>
2025-01-30 17:16:56 +0700

    restructuring folders for gitops

--- argocd.tf
+++ argocd.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace" "argocd" {
 
 locals {
   # provider based function
-  install_manifests = provider::kubernetes::manifest_decode_multi(file("argocd-install/manifests/argocd-install-v2.13.3.yaml"))
+  install_manifests = provider::kubernetes::manifest_decode_multi(file("argocd/install-manifests/argocd-install-v2.13.3.yaml"))
 }
 
 resource "kubernetes_manifest" "argocd_install" {
@@ -16,4 +16,10 @@ resource "kubernetes_manifest" "argocd_install" {
   }  
   manifest = each.value
   depends_on = [ kubernetes_namespace.argocd ]
+}
+
+# Multiple files in a folder to be applied
+resource "kubernetes_manifest" "manifests" {
+  for_each = fileset("argocd/bootstrap-apps/", "*.yaml")
+  manifest = yamldecode(file("argocd/bootstrap-apps/${each.value}"))
 }
