7b842ffb736ea1476cf3af7055ee4a312b5e260e
Paolo Carta <ing.pcarta@gmail.com>
2025-01-30 16:47:03 +0700

    cleanup argocd from tf

--- argocd-install/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket          = "interview-prep-paolos-terraform-remote-state"
-        key             = "envs/test/k8s-clusters.tfstate"
-        region          = "ap-southeast-1"
-        encrypt         = true
-        dynamodb_table  = "interview-prep-paolos-terraform-remote-state-lock"
-    }
-}
